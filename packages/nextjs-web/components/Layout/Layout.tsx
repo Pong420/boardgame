f54c680d9b10ce6b3c90645c13d1e202fe335a50
Pong <samfunghp@gmail.com>
2020-09-15 09:43:58 +0800

    feat(nextjs-web): add activiate handling

--- packages/nextjs-web/components/Layout/Layout.tsx
+++ packages/nextjs-web/components/Layout/Layout.tsx
@@ -1,6 +1,7 @@
 import React, { ReactNode } from 'react';
 import styles from './Layout.module.scss';
 import { usePreferencesState } from '@/services';
+import { useActivate } from '@/hooks/useActivate';
 
 interface LayoutProps {
   children?: ReactNode;
@@ -8,6 +9,8 @@ interface LayoutProps {
 
 export const Layout = ({ children }: LayoutProps) => {
   const { screenWidth } = usePreferencesState();
+
+  useActivate();
 
   return (
     <div
---
+++ packages/nextjs-web/hooks/useActivate.tsx
@@ -0,0 +1,30 @@
+import router, { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import { fromEvent } from 'rxjs';
+import { filter } from 'rxjs/operators';
+import { createLocalStorage } from '@/utils/storage';
+import { JSONParse } from '@/utils/JSONParse';
+
+const activate = createLocalStorage<number>('BOARDGAME_ACTIVATE', 0);
+
+export function useActivate() {
+  const { pathname, asPath } = useRouter();
+  const isErrorPage = [pathname, asPath].some(p => p.startsWith('/error'));
+
+  useEffect(() => {
+    const subscription = fromEvent<StorageEvent>(window, 'storage')
+      .pipe(filter(event => event.key === activate.key))
+      .subscribe(event => {
+        const newValue = event.newValue && JSONParse<number>(event.newValue);
+        // If newValue is not a number. May be deleted by the user
+        if (typeof newValue === 'number') {
+          router.push('/error/Only allow one screen at a time');
+        }
+      });
+
+    if (!isErrorPage) {
+      activate.save(+new Date());
+    }
+    return () => subscription.unsubscribe();
+  }, [isErrorPage]);
+}
