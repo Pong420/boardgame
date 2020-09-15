c305151fa1c1950e97e0484693d913dde6185e9d
Pong <samfunghp@gmail.com>
2020-09-15 19:26:47 +0800

    fix(web): add storage support checking

--- packages/web/components/Layout.tsx
+++ packages/web/components/Layout.tsx
@@ -1,5 +1,7 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useEffect } from 'react';
+import router from 'next/router';
 import { useActivate } from '@/hooks/useActivate';
+import { storageSupport } from '@/utils/storage';
 
 interface LayoutProps {
   children?: ReactNode;
@@ -7,6 +9,13 @@ interface LayoutProps {
 
 export const Layout = ({ children }: LayoutProps) => {
   useActivate();
+
+  useEffect(() => {
+    !storageSupport() &&
+      router.push(
+        '/error/Sorry, The website relies on local storage. You should enable it to continue '
+      );
+  }, []);
 
   return <div className="layout">{children}</div>;
 };
