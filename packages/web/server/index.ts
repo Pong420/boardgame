993e19fa7f2697f0dd34c9d04772ea2dd34c3210
Pong <samfunghp@gmail.com>
2020-09-17 00:33:06 +0800

    fix(web): adjust for testing

--- packages/web/server/index.ts
+++ packages/web/server/index.ts
@@ -1,33 +1,6 @@
-import path from 'path';
-import next from 'next';
-import { server } from './bgio-server';
+import { startServer } from './startServer';
 
 const dev = process.env.NODE_ENV !== 'production';
-const app = next({ dev, dir: path.join(__dirname, '../') });
-const handle = app.getRequestHandler();
 const port = Number(process.env.PORT) || 3000;
 
-(async () => {
-  try {
-    await app.prepare();
-
-    server.router.all('*', async ctx => {
-      await handle(ctx.req, ctx.res);
-      ctx.respond = false;
-    });
-
-    server.app.use(async (ctx, next) => {
-      ctx.res.statusCode = 200;
-      await next();
-    });
-
-    server.run(port, (err?: any) => {
-      if (err) throw err;
-      // eslint-disable-next-line
-      console.log(`> Ready on localhost:${port} - env ${process.env.NODE_ENV}`);
-    });
-  } catch (e) {
-    console.error(e);
-    process.exit(1);
-  }
-})();
+startServer({ dev, port });
