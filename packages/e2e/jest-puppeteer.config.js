4e871144467cd96b98e93074a22fe837b9668d34
Pong <samfunghp@gmail.com>
2020-09-19 23:59:13 +0800

    fix(actions): test

--- packages/e2e/jest-puppeteer.config.js
+++ packages/e2e/jest-puppeteer.config.js
@@ -3,7 +3,7 @@ module.exports = {
     // If true disable ui
     headless: process.env.HEADLESS !== 'false' && process.env.CI !== 'true',
     defaultViewport: null,
-    args: ['--no-sandbox']
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
   },
   exitOnPageError: false
 };
