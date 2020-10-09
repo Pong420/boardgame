2debc8c09d56f6bcefb0fcc7210408528880080b
Pong <samfunghp@gmail.com>
2020-10-09 16:53:35 +0800

    fix(server): e2e test

--- packages/server/test/app.e2e-spec.ts
+++ packages/server/test/app.e2e-spec.ts
@@ -1,24 +1,52 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { MongoMemoryServer } from 'mongodb-memory-server-global';
+import {
+  FastifyAdapter,
+  NestFastifyApplication
+} from '@nestjs/platform-fastify';
+import { AppModule, setupApp } from './../src/app.module';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { MatchService } from '@/match/match.service';
 
 describe('AppController (e2e)', () => {
-  let app: INestApplication;
+  let app: NestFastifyApplication;
+  const mongod = new MongoMemoryServer({ autoStart: false });
+  const gameName = 'gameName';
+
+  beforeAll(async () => {
+    await mongod.start();
+  });
+
+  afterAll(async () => {
+    await mongod.stop();
+  });
 
   beforeEach(async () => {
+    const mongoUri = await mongod.getUri();
+
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule]
+      imports: [AppModule.init({ games: [{ name: gameName }], mongoUri })]
     }).compile();
 
-    app = moduleFixture.createNestApplication();
+    app = moduleFixture.createNestApplication<NestFastifyApplication>(
+      new FastifyAdapter()
+    );
+
+    setupApp(app);
+
     await app.init();
+    await app.getHttpAdapter().getInstance().ready();
+  });
+
+  afterEach(async () => {
+    await app.close();
+    await app.get(MatchService).disconnect();
   });
 
   it('/ (GET)', () => {
     return request(app.getHttpServer())
-      .get('/')
+      .get(`/api/match/${gameName}?name=${gameName}`)
       .expect(200)
-      .expect('Hello World!');
+      .expect({ matches: [] });
   });
 });
